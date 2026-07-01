3d91d2df0dfcd6211abd080598d8546da6653489
alshryhy <assan757@gmail.com>
2026-07-01 05:10:31 +0300

    Clean temporary files and restore widget sources

---
+++ ios/AlzidanFamilyWidget/AlzidanFamilyWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  AlzidanFamilyWidgetBundle.swift
+//  AlzidanFamilyWidget
+//
+//  Created by ahshryhy on 15/01/1448 AH.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct AlzidanFamilyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        AlzidanFamilyWidget()
+    }
+}
